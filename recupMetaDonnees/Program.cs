3688e1306cd5ff89b4228cad8fbc1a8967d098d7
Collaborateur Externe <collab.ext@axiome-solution.fr>
2018-04-30 10:37:54 +0200

    Add project files.

---
+++ recupMetaDonnees/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using recupMetaDonnees;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recupMetaDonnees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// String Variable to store the siteURL
+            string URL = "http://loca-mle-w16:81/sites/testBot/compta";
+            string nomListe = "Facture";
+            string nomContentType = "Facture";
+
+            //// Get the context for the SharePoint Site to access the data
+            ClientContext clientContext = new ClientContext(URL);
+
+            //// Get the content type collection for the list "Custom"
+            //ContentTypeCollection contentTypeColl = clientContext.Web.Lists.GetByTitle(nomListe).ContentTypes;
+            ContentTypeCollection contentTypeColl = RecupDonneesSharePoint.recupContentTypeDuneList(clientContext, nomListe);
+
+            clientContext.Load(contentTypeColl);
+            clientContext.ExecuteQuery();
+
+            FieldCollection fC = RecupDonneesSharePoint.recupChampDunContentType(clientContext, contentTypeColl, nomContentType);
+            //// Display the Content Type name
+            
+            foreach (Field f in fC)
+            {
+                Console.WriteLine(f.Title);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ recupMetaDonnees/RecupDonneesSharePoint.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recupMetaDonnees
+{
+    class RecupDonneesSharePoint
+    {
+        public static ContentTypeCollection recupContentTypeDuneList(ClientContext clientContext, string nomListe)
+        {
+            //// Get the content type collection for the list "Custom"
+            ContentTypeCollection contentTypeColl = clientContext.Web.Lists.GetByTitle(nomListe).ContentTypes;
+
+            clientContext.Load(contentTypeColl);
+            clientContext.ExecuteQuery();
+
+            return contentTypeColl;
+        }
+
+        public static FieldCollection recupChampDunContentType(ClientContext clientContext, ContentTypeCollection contentTypeColl, string nomContentType)
+        {
+            FieldCollection fC=null;
+            //// Get the content type collection for the list "Custom"
+            foreach (ContentType ct in contentTypeColl)
+            {
+                if(ct.Name==nomContentType)
+                {
+                fC = ct.Fields;
+                clientContext.Load(fC);
+                clientContext.ExecuteQuery();
+                }
+
+            }
+            return fC;
+        }
+    }
+}
